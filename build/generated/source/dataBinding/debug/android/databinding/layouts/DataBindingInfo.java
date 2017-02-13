b9d64689b304dbdc69867a6ecbf0d2d63880b628
Tamzid Rahman Oronno <tamzid.gregorian@gmail.com>
2017-02-13 22:55:42 +0600

    add via up

---
+++ build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="7e3955b6-47ce-4fa1-ad3b-d330bb3d5ec3", modulePackage="com.example.tamzid.baymax", sdkRoot="G:\\Android SDK\\sdk",layoutInfoDir="C:\\Users\\TAMZID\\AndroidStudioProjects\\Baymax\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=15,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
